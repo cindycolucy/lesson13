3eb446813ba3ca64507786873027dfe16bbe4989
Cindy Colucy <ckcolucy@gmail.com>
2017-09-21 11:27:34 -0400

    "adding lambda expression"

--- src/TestPayIncreaseLambda.java
+++ src/TestPayIncreaseLambda.java
@@ -1,39 +1,45 @@
 import payincrease.*;
 
 public class TestPayIncreaseLambda {
-	
-  public static void main(String[] args) {
 
-        Person workers[] = new Person[3];
+	public static void main(String[] args) {
+
+		Person workers[] = new Person[4];
 		workers[0] = new Person("John", 'E');
 		workers[1] = new Person("Mary", 'C');
-		workers[2] = new Person("Steve", 'E');		
+		workers[2] = new Person("Steve", 'E');
+		workers[3] = new Person("Boris", 'F');
 
-	  // Lambda expression for increasing Employee's pay
-	   Payable increaseRulesEmployee = (int percent) -> {
-				return true;
-	   };
-	   
-		// Lambda expression for increasing Contractor's pay	   
-	    Payable increaseRulesContractor = (int percent) -> {
-	    	if(percent > Payable.INCREASE_CAP){
-	    		System.out.print(" Sorry, can't increase hourly rate by more than " + 
-	    	             Payable.INCREASE_CAP + "%. "); 
+		// Lambda expression for increasing Employee's pay
+		Payable increaseRulesEmployee = (int percent) -> {
+			return true;
+		};
+
+		Payable increaseRulesForeignWorker = (int percent) -> {
+			System.out.println("I'm just a foreign worker...");
+			return true;
+		};
+
+		// Lambda expression for increasing Contractor's pay
+		Payable increaseRulesContractor = (int percent) -> {
+			if (percent > Payable.INCREASE_CAP) {
+				System.out.print(" Sorry, can't increase hourly rate by more than " + Payable.INCREASE_CAP + "%. ");
 				return false;
-			} else {	
+			} else {
 				return true;
 			}
-	   };	
-	   
-	   
-	   for (Person p: workers){
-		   if ('E'==p.getWorkerStatus()){
-			   // Validate 30% increase for every worker
-			   p.validatePayIncrease(increaseRulesEmployee, 30); 
-		   } else if ('C'==p.getWorkerStatus()){
-			   p.validatePayIncrease(increaseRulesContractor, 30);
-		   }
-	   }	   
-  }
+		};
+
+		for (Person p : workers) {
+			if ('E' == p.getWorkerStatus()) {
+				// Validate 30% increase for every worker
+				p.validatePayIncrease(increaseRulesEmployee, 30);
+			} else if ('C' == p.getWorkerStatus()) {
+				p.validatePayIncrease(increaseRulesContractor, 30);
+			} else if ('F' == p.getWorkerStatus()) {
+				p.validatePayIncrease(increaseRulesForeignWorker, 30);
+			}
+		}
+	}
 
 }
